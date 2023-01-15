d846783b01e859544f0bee2c61c71ce9bcc6bc16
odionjulius7 <odionjulius7@gmail.com>
2023-01-15 06:23:49 +0100

    new commit by julius

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "remixicon/fonts/remixicon.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+import "./index.css";
+import App from "./App";
 
 /*
 REACT BOOTSTRAP CONFIGURATION
 */
 
-import '../node_modules/react-bootstrap/dist/react-bootstrap'
+import "../node_modules/react-bootstrap/dist/react-bootstrap";
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-
-
-
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ToastContainer autoClose={3000} />
     <App />
   </React.StrictMode>
 );
-
